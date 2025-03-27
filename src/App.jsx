4111f6c61cd277dcc0f1600faf044d612a656f44
alexciccarese <alex.ciccarese@gmail.com>
2025-03-27 14:35:53 +0100

    add search functionality to filter movie list

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,51 @@
 import { useEffect, useState } from 'react'
 
 const movies = [
-  { title: 'Inception', genre: 'Fantascienza' },
-  { title: 'Il Padrino', genre: 'Thriller' },
-  { title: 'Titanic', genre: 'Romantico' },
-  { title: 'Batman', genre: 'Azione' },
-  { title: 'Interstellar', genre: 'Fantascienza' },
-  { title: 'Pulp Fiction', genre: 'Thriller' },
+  { title: 'Inception', genere: 'Fantascienza' },
+  { title: 'Il Padrino', genere: 'Thriller' },
+  { title: 'Titanic', genere: 'Romantico' },
+  { title: 'Batman', genere: 'Azione' },
+  { title: 'Interstellar', genere: 'Fantascienza' },
+  { title: 'Pulp Fiction', genere: 'Thriller' },
 ]
 
 
 function App() {
+  const [filteredMovie, setFilteredMovie] = useState(movies)
+  const [search, setSearch] = useState('')
 
+  useEffect(() => {
+    setFilteredMovie(movies.filter(movie => movie.title.toLowerCase().includes(search.toLowerCase())))
+  }, [search])
 
   return (
     <>
-    <div className="container mt-4">
-      <h1>Lista dei Film</h1>
+      <div className="container mt-4">
+        <h1>Lista dei Film</h1>
 
-        <ul className="list-group mt-3">
+        <label className="form-label">Cerca per titolo</label>
+        <input
+          type="text"
+          name="search-movie"
+          id="search-movie"
+          className="form-control"
+          aria-describedby="movie-helper"
+          placeholder='Cerca titolo qui'
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
 
-      {movies.map(movie => (
-        <li className="list-group-item mb-2">{movie.title}</li>
-      ))}
 
-      </ul>
-    </div>
+
+        <ul className="list-group mt-5">
+          {filteredMovie.map((movie, index) => (
+            <li key={`movie-${index}`} className="list-group-item mb-2">
+              {movie.title}
+            </li>
+          ))}
+        </ul>
+
+      </div>
     </>
   )
 }
