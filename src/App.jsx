282b4b3c5a0a01f3cd48916a80dfa7dfce1d0343
alexciccarese <alex.ciccarese@gmail.com>
2025-03-27 13:32:24 +0100

    add movie list display to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+const movies = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
 
 
 function App() {
 
-  
+
   return (
     <>
+    <div className="container mt-4">
+      <h1>Lista dei Film</h1>
 
+        <ul className="list-group mt-3">
+
+      {movies.map(movie => (
+        <li className="list-group-item mb-2">{movie.title}</li>
+      ))}
+
+      </ul>
+    </div>
     </>
   )
 }
