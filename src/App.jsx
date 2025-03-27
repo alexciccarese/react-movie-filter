4b88792c9877577947d47cb1daae1617f7a8e7c4
alexciccarese <alex.ciccarese@gmail.com>
2025-03-27 15:41:52 +0100

    add form to submit new movies with genre selection

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,18 @@ function App() {
   const [filteredMovie, setFilteredMovie] = useState(movies)
   const [search, setSearch] = useState('')
   const [genere, setGenere] = useState('')
+  const [newMovie, setNewMovie] = useState('')
+  const [addGenere, setAddGenere] = useState('')
 
   useEffect(() => {
     setFilteredMovie(movies.filter(movie => movie.title.toLowerCase().includes(search.toLowerCase()) && movie.genere.includes(genere)))
 
   }, [search, genere])
 
+
+  function handleFormSubmit(e) {
+    e.preventDefault(e)
+  }
 
 
   return (
@@ -38,12 +44,12 @@ function App() {
           onChange={e => setSearch(e.target.value)}
         />
 
-        <select 
-        className="form-select form-select-lg mt-3" 
-        aria-label="genere-helper"
-        id='genere-movie'
-        value={genere}
-        onChange={e => setGenere(e.target.value)}
+        <select
+          className="form-select mt-3"
+          aria-label="genere-helper"
+          id='genere-movie'
+          value={genere}
+          onChange={e => setGenere(e.target.value)}
 
         >
           <option value="">Seleziona genere (nessuna selezoione)</option>
@@ -53,9 +59,49 @@ function App() {
           <option value="Azione">Azione</option>
         </select>
 
+        <hr />
+
+        <form onSubmit={handleFormSubmit}>
+          <div className="mb-3 align-items-center">
+
+            <label className="form-label">Aggiungi un nuovo film</label>
+            <div className='d-flex gap-3'>
+              <input
+                type="text"
+                className="form-control"
+                name="new-movie"
+                id="new-movie"
+                aria-describedby="new-movie-helper"
+                placeholder="Inserisci il titolo del film"
+                value={newMovie}
+                onChange={e => setNewMovie(e.target.value)}
+              />
+              <select
+                className="form-select form-select"
+                aria-label="new-genere-helper"
+                id='new-genere-movie'
+                value={addGenere}
+                onChange={e => setAddGenere(e.target.value)}
+              >
+                <option value="">Seleziona genere (nessuna selezoione)</option>
+                <option value="Fantascienza">Fantascienza</option>
+                <option value="Thriller">Thriller</option>
+                <option value="Romantico">Romantico</option>
+                <option value="Azione">Azione</option>
+              </select>
+            </div>
+
+            <button type="submit" className="btn btn-primary mt-2">
+              Submit
+            </button>
+          </div>
 
 
-        <ul className="list-group mt-5">
+        </form >
+
+
+        <h2 className='mt-5'>I tuoi film:</h2>
+        <ul className="list-group mt-2">
           {filteredMovie.map((movie, index) => (
             <li key={`movie-${index}`} className="list-group-item mb-2">
               {movie.title}
@@ -63,7 +109,7 @@ function App() {
           ))}
         </ul>
 
-      </div>
+      </div >
     </>
   )
 }
